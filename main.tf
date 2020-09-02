0ad7781ef431b283b0f9dc11cb242bf05c5ae696
Sukanta Mondal <60913612+mondalsukanta@users.noreply.github.com>
2020-09-01 20:37:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "subnet" {
     name = "${var.prefix}Subnet"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.0.0/24"]
+    address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IP
