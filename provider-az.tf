f5ac69cc54fc73845a6b5a90f131fe655297e676
Sukanta Mondal <60913612+mondalsukanta@users.noreply.github.com>
2020-09-02 07:01:32 -0500

    Update provider-az.tf

--- provider-az.tf
+++ provider-az.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
-  version = "~> 1.23"
+  version = "~> 2.1.0"
   features {}
 }
