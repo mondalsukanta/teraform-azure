b02d91b67635440ec066e3a60dc84770ce6ceb7a
Sukanta Mondal <60913612+mondalsukanta@users.noreply.github.com>
2020-09-02 21:18:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,171 +1,73 @@
-# Configure the Microsoft Azure Provider
+## <https://www.terraform.io/docs/providers/azurerm/index.html>
 provider "azurerm" {
-  # We recommend pinning to the specific version of the Azure Provider you're using
-  # since new versions are released frequently
-  version = "=2.20.0"
-
+  version = "=2.5.0"
   features {}
-
-  # More information on the authentication methods supported by
-  # the AzureRM Provider can be found here:
-  # http://terraform.io/docs/providers/azurerm/index.html
-
-  # subscription_id = "..."
-  # client_id       = "..."
-  # client_secret   = "..."
-  # tenant_id       = "..."
 }
 
-# Create Resource Group
+## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
 resource "azurerm_resource_group" "rg" {
-    name = "Terra-K8s-Practise"
-    location = var.location   
-    tags = var.tags
+  name     = "TerraformTesting"
+  location = "eastus"
 }
 
-# Create Vitural Network
+## <https://www.terraform.io/docs/providers/azurerm/r/availability_set.html>
+resource "azurerm_availability_set" "DemoAset" {
+  name                = "example-aset"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/virtual_network.html>
 resource "azurerm_virtual_network" "vnet" {
-    name = "${var.prefix}Vnet"
-    address_space = ["10.0.0.0/16"]
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    tags = var.tags
+  name                = "vNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Create Subnet 
+## <https://www.terraform.io/docs/providers/azurerm/r/subnet.html> 
 resource "azurerm_subnet" "subnet" {
-    name = "${var.prefix}Subnet"
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.0.0/24"]
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
 }
 
-# Create public IP
-resource "azurerm_public_ip" "publicip" {
-  name                = "${var.prefix}PublicIP"
-  location            = var.location
+## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-  tags                = var.tags
-}
-
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.prefix}NSG"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-  security_rule {
-    name                       = "SSH-VPN"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "14.140.98.138/32"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "K8s-VPN-ports"
-    priority                   = 1010
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "30000-32767"
-    source_address_prefix      = "14.140.98.138/32"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "SSH-direct"
-    priority                   = 1015
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "49.207.208.185/32"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "K8s-direct-ports"
-    priority                   = 1020
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "30000-32767"
-    source_address_prefix      = "49.207.208.185/32"
-    destination_address_prefix = "*"
-  }
-}
-
-# Create network interface
-resource "azurerm_network_interface" "nic" {
-  name                      = "${var.prefix}NIC"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  tags                      = var.tags
 
   ip_configuration {
-    name                          = "${var.prefix}NICConfg"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
-#resource "azurerm_network_interface_security_group_association" "nic-nsg" {
-#  network_interface_id      = azurerm_network_interface.nic.id
-#  network_security_group_id = azurerm_network_security_group.nsg.id
-#}
+## <https://www.terraform.io/docs/providers/azurerm/r/windows_virtual_machine.html>
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  availability_set_id = azurerm_availability_set.DemoAset.id
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
-# Create a Linux virtual machine
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "${var.prefix}-Master"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_D2S_v3"
-  tags                  = var.tags
-
-  storage_os_disk {
-    name              = "${var.prefix}OsDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = lookup(var.sku, var.location)
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
     version   = "latest"
   }
-
-  os_profile {
-    computer_name  = "${var.prefix}TFVM"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-}
-
-data "azurerm_public_ip" "ip" {
-  name                = azurerm_public_ip.publicip.name
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  depends_on = [azurerm_virtual_machine.vm]
-}
-
-output "os_sku" {
-  value = lookup(var.sku, var.location)
-}
-
-output "public_ip_address" {
-  value = data.azurerm_public_ip.ip.ip_address
 }
